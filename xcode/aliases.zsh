1593ef99b0c79e0b32a6419a297ffe671b444ffb
Edward Huynh <edward@edwardhuynh.com>
2014-06-28 23:07:35 +1000

    added aliases for Xcode

---
+++ xcode/aliases.zsh
@@ -0,0 +1,12 @@
+# iphone simulator
+alias ios-sim="open /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/Applications/iPhone\ Simulator.app"
+
+# open the current directory with Xcode
+alias x="open -a 'Xcode' ."
+alias x-pre="open -a 'Xcode6-DP' ."
+
+# clear the derived data folder
+alias fuxcode='rm -rf ~/Library/Developer/Xcode/DerivedData/'
+
+# cocoapods
+alias pi="pod install"
