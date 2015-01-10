6a3e70ffb9b43951c29ddcac66b5b2393b4db37f
Edward Huynh <edward@edwardhuynh.com>
2015-01-10 17:49:28 +1100

    install soda theme as part of the sublime setup

--- sublime/install.sh
+++ sublime/install.sh
@@ -1,14 +1,30 @@
-echo "installing sublime settings"
-
 # sublime dir
 sublime_dir=~/Library/Application\ Support/Sublime\ Text\ 2/Packages
 
-# symlink the User settings
-for source in `find $DOTFILES/sublime/User -name \*.\*`
-do
-	file=`basename $source`
-	rm "$sublime_dir/User/$file"
-	ln -s $source "$sublime_dir/User/$file"
-done
+init_user_settings () {
+		# symlink the User settings
+	for source in `find $DOTFILES/sublime/User -name \*.\*`
+	do
+		file=`basename $source`
+		rm "$sublime_dir/User/$file"
+		ln -s $source "$sublime_dir/User/$file"
+	done
+}
 
-echo "completed installing sublime settings"
+init_soda_theme () {
+	soda_dir="$sublime_dir/Theme - Soda"
+
+	if [ ! -d "$soda_dir" ]; then
+		echo "its not there"
+		git clone https://github.com/buymeasoda/soda-theme/ "$soda_dir"
+	fi
+}
+
+if [ -d "$sublime_dir" ]; then
+	echo "installing sublime settings"
+
+	init_user_settings
+	init_soda_theme
+
+	echo "completed installing sublime settings"
+fi
