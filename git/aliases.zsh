94ed2801807b632014881ee83549b34d63d7bc8d
Edward Huynh <edward@edwardhuynh.com>
2015-02-10 14:36:12 +1100

    removed aliases i nevered used

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -14,16 +14,6 @@ alias gco='git checkout'
 
 alias gre='git rebase'
 
-# stashing
-alias gst='git stash'
-alias gstpush='git stash push'
-alias gstpop='git stash pop'
-
-# reset
-alias gr='git reset'
-alias grhcc='git reset --hard HEAD' # Reset HARD to current commit
-alias grmpc='git reset --mixed HEAD~1' # Reset MIXED to previous commit
-
 # diffs
 alias gd='git diff' # Changes in the working tree not yet staged for the next commit.
 alias gds='git diff --cached' # Changes between the index and your last commit.
