f47feeba92fc0cc72ba986b4010ff55a83d7a712
Edward Huynh <edward@edwardhuynh.com>
2015-11-18 14:03:14 +1100

    download solarized theme for vim

--- vim/vim.sh
+++ vim/vim.sh
@@ -7,6 +7,21 @@ install_vundle () {
 	fi
 }
 
+install_theme () {
+  mkdir -p "$HOME/code/vim"
+
+  solarized_theme_dir="$HOME/code/vim/vim-colors-solarized"
+
+  if [ ! -d "$solarized_theme_dir" ]; then
+    echo "downloading solarized theme"
+    git clone https://github.com/altercation/vim-colors-solarized.git "$solarized_theme_dir"
+    echo "installing solarized theme"
+    mkdir -p ~/.vim/colors/
+    cp $solarized_theme_dir/colors/solarized.vim ~/.vim/colors/
+  fi
+}
+
 mkdir -p "$HOME/.vim/bundle"
 
 install_vundle
+install_theme
