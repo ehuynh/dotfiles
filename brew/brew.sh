8a7d8ad5c319b3fcc89410ce897f3699a0e2bb4d
Edward Huynh <edward@edwardhuynh.com>
2016-08-12 17:32:58 +1000

    remove brew cask

--- brew/brew.sh
+++ brew/brew.sh
@@ -12,32 +12,6 @@ fi
 echo "Installing Chisel"
 brew install chisel
 
-echo "Installing Cask"
-brew install caskroom/cask/brew-cask
-
-echo "Installing Reveal"
-brew cask install reveal
-
-echo "Installing Dash"
-brew cask install dash
-
-echo "Installing sublime-text"
-brew cask install sublime-text
-
-echo "Installing iterm"
-brew cask install iterm2
-
-echo "Installing sourcetree"
-brew cask install sourcetree
-
-echo "Installing flux"
-brew cask install flux
-
 echo "Installing Carthage"
 brew install carthage
 
-echo "Installing Charles Proxy"
-brew cask install charles
-
-echo "Installing MacDown"
-brew cask install macdown
