91574875e3211a35f0856260ff3996241ab51544
Edward Huynh <edward@edwardhuynh.com>
2016-09-05 11:38:33 +1000

    add function to open a pr in github

---
+++ git/functions.zsh
@@ -0,0 +1,15 @@
+pr() {
+    open "https://github.com/$(org_name)/$(repo_name)/pull/new/$(branch_name)"
+}
+
+org_name() {
+ git remote -v | grep -m 1 "(push)" | sed -e "s/.*github.com[:/]\(.*\)\/.*.git.*/\1/"
+}
+
+repo_name() {
+  git remote -v | grep -m 1 "(push)" | sed -e "s/.*github.com[:/].*\/\(.*\)\.git.*/\1/"
+}
+
+branch_name() {
+  git name-rev --name-only HEAD
+}
