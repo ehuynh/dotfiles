3e6e80f0fd08bf68fe21197dbe8b4b021a539624
Edward Huynh <edward@edwardhuynh.com>
2016-11-11 12:15:52 +1100

    modify and add new history configurations

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -1,36 +1,31 @@
-# location of history file
-HISTFILE=~/.history
+#
+# Variables
+#
 
-# number of commands to remember
-HISTSIZE=2000
+HISTFILE="${ZDOTDIR:-$HOME}/.zhistory"       # The path to the history file.
+HISTSIZE=10000                   # The maximum number of events to save in the internal history.
+SAVEHIST=10000                   # The maximum number of events to save in the history file.
 
-# number of commands to save
-SAVEHIST=$HISTSIZE
+#
+# Options
+#
 
-# zsh sessions will append their history list to the history file, rather than replace it. 
-# multiple parallel zsh sessions will all have the new entries from their history lists added to the history file, 
-# in the order that they exit.
-setopt APPEND_HISTORY
+setopt BANG_HIST                 # Treat the '!' character specially during expansion.
+setopt EXTENDED_HISTORY          # Write the history file in the ':start:elapsed;command' format.
+setopt INC_APPEND_HISTORY        # Write to the history file immediately, not when the shell exits.
+setopt SHARE_HISTORY             # Share history between all sessions.
+setopt HIST_EXPIRE_DUPS_FIRST    # Expire a duplicate event first when trimming history.
+setopt HIST_IGNORE_DUPS          # Do not record an event that was just recorded again.
+setopt HIST_IGNORE_ALL_DUPS      # Delete an old recorded event if a new event is a duplicate.
+setopt HIST_FIND_NO_DUPS         # Do not display a previously found event.
+setopt HIST_IGNORE_SPACE         # Do not record an event starting with a space.
+setopt HIST_SAVE_NO_DUPS         # Do not write a duplicate event to the history file.
+setopt HIST_VERIFY               # Do not execute immediately upon history expansion.
+setopt HIST_BEEP                 # Beep when accessing non-existent history.
 
-# imports new commands from the history file, 
-# and also causes your typed commands to be appended to the history fileshare history between multiple shells 
-# straigh away, as oppose to when the session ends.
-setopt SHARE_HISTORY
+#
+# Aliases
+#
 
-# If I type cd and then cd again, only save the last one
-setopt HIST_IGNORE_DUPS
-
-# If a new command line being added to the history list duplicates an older one, 
-# the older command is removed from the list (even if it is not the previous event)
-setopt HIST_IGNORE_ALL_DUPS
-
-# When writing out the history file, older commands that duplicate newer ones are omitted
-setopt HIST_SAVE_NO_DUPS
-
-# Pretty    Obvious.  Right?
-setopt HIST_REDUCE_BLANKS
-
-# If the internal history needs to be trimmed to add the current command line, 
-# setting this option will cause the oldest history event that has a duplicate to be lost 
-# before losing a unique event from the list
-setopt HIST_EXPIRE_DUPS_FIRST
+# Lists the ten most used commands.
+alias history-stat="history 0 | awk '{print \$2}' | sort | uniq -c | sort -n -r | head"
