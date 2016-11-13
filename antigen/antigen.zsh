770210711b00b9286e90a7054bca6abe305bd767
Edward Huynh <edward@edwardhuynh.com>
2016-11-13 18:14:41 +1100

    add extra zsh completions

--- antigen/antigen.zsh
+++ antigen/antigen.zsh
@@ -4,6 +4,9 @@ source $(brew --prefix)/share/antigen/antigen.zsh
 # add zsh-autosuggestions
 antigen bundle zsh-users/zsh-autosuggestions
 
+# add extra completions to zsh
+antigen bundle zsh-users/zsh-completions
+
 # add syntax highlighting (this must be last out of all the bundles)
 antigen bundle zsh-users/zsh-syntax-highlighting
 
