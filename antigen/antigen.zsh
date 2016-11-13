d22e31713649166d8647a24fba685451cac1d113
Edward Huynh <edward@edwardhuynh.com>
2016-11-13 17:45:59 +1100

    add antigen and add zsh-autosuggestion via antigen

---
+++ antigen/antigen.zsh
@@ -0,0 +1,13 @@
+# load antigen
+source $(brew --prefix)/share/antigen/antigen.zsh
+
+# add zsh-autosuggestions
+antigen bundle zsh-users/zsh-autosuggestions
+
+# add syntax highlighting (this must be last out of all the bundles)
+antigen bundle zsh-users/zsh-syntax-highlighting
+
+# Tell antigen that you're done.
+antigen apply
+
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=30'
