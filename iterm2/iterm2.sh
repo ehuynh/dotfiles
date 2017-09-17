26ed7b496c223e94e7ad3369b3209572c5384018
Edward Huynh <ehuynh@seek.com.au>
2017-09-17 17:02:05 +1000

    download iterm themes

--- iterm2/iterm2.sh
+++ iterm2/iterm2.sh
@@ -1,13 +1,13 @@
 install_theme () {
   mkdir -p "$HOME/code/iterm2"
 
-  solarized_theme_dir="$HOME/code/iterm2/solarized"
+  iterm_theme_dir="$HOME/code/iterm2/themes"
 
-  if [ ! -d "$solarized_theme_dir" ]; then
-    echo "downloading solarized theme"
-    git clone https://github.com/altercation/solarized.git "$solarized_theme_dir"
-    echo "installing solarized theme"
-    open "$solarized_theme_dir/iterm2-colors-solarized/Solarized Dark.itermcolors"
+  if [ ! -d "$iterm_theme_dir" ]; then
+    echo "downloading iterm themes"
+    git clone git@github.com:mbadolato/iTerm2-Color-Schemes.git "$iterm_theme_dir"
+    # echo "installing solarized theme"
+    # open "$solarized_theme_dir/iterm2-colors-solarized/Solarized Dark.itermcolors"
   fi
 }
 
