4f635ee78c5bddca28cf11e6784f974da912dc97
Edward Huynh <ehuynh@seek.com.au>
2018-02-19 15:27:29 +1100

    add xcode snippet to generate documentation template

--- xcode/xcode.sh
+++ xcode/xcode.sh
@@ -1,2 +1,6 @@
 rm ~/Library/Developer/Xcode/UserData/xcdebugger/Breakpoints_v2.xcbkptlist
+mkdir -p ~/Library/Developer/Xcode/UserData/xcdebugger
 ln -s $DOTFILES/xcode/Breakpoints_v2.xcbkptlist ~/Library/Developer/Xcode/UserData/xcdebugger/Breakpoints_v2.xcbkptlist
+
+rm -rf ~/Library/Developer/Xcode/UserData/CodeSnippets
+ln -s $DOTFILES/xcode/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets
