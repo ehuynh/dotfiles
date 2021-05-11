8a57b9a92b4a0b1542794b3e3d7340b3b5be349b
Edward Huynh <edward.huynh@rea-group.com>
2021-05-11 10:19:39 +1000

    fix git functions to work with different hosts

--- git/functions.zsh
+++ git/functions.zsh
@@ -1,17 +1,21 @@
 pr() {
-    open "https://github.com/$(org_name)/$(repo_name)/pull/new/$(branch_name)"
+    open "https://$(host_name)/$(org_name)/$(repo_name)/pull/new/$(branch_name)"
 }
 
 gpr() {
   git fetch origin && git reset --hard origin/$(branch_name)
 }
 
+host_name() {
+  git remote -v | grep -m 1 "(push)" | sed -e "s/.*git@\(.*\)[:/].*\/.*.git.*/\1/"  
+}
+
 org_name() {
- git remote -v | grep -m 1 "(push)" | sed -e "s/.*github.com[:/]\(.*\)\/.*.git.*/\1/"
+  git remote -v | grep -m 1 "(push)" | sed -e "s/.*[:/]\(.*\)\/.*.git.*/\1/"
 }
 
 repo_name() {
-  git remote -v | grep -m 1 "(push)" | sed -e "s/.*github.com[:/].*\/\(.*\)\.git.*/\1/"
+  git remote -v | grep -m 1 "(push)" | sed -e "s/.*[:/].*\/\(.*\)\.git.*/\1/"
 }
 
 branch_name() {
