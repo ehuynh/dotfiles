d7eba35941c6806e8ac3ef177d46981b6d0933e7
Edward Huynh <edward.huynh@rea-group.com>
2024-09-10 11:03:20 +1000

    temp

--- powerline/powerline.zsh
+++ powerline/powerline.zsh
@@ -5,4 +5,4 @@ POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(vcs)
 POWERLEVEL9K_VCS_GIT_HOOKS=(git-remotebranch)
 
 POWERLEVEL9K_MODE='nerdfont-complete'
-source /usr/local/opt/powerlevel9k/powerlevel9k.zsh-theme
+source /opt/homebrew/opt/powerlevel9k/powerlevel9k.zsh-theme
